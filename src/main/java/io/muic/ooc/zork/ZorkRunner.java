49585f5e344903bbd94535ce50df5be54386283f
gigadot <gigadot@gmail.com>
2017-01-12 15:26:12 +0700

    zork game starter

---
+++ src/main/java/io/muic/ooc/zork/ZorkRunner.java
@@ -0,0 +1,26 @@
+package io.muic.ooc.chatbot;
+
+import java.util.Scanner;
+
+/**
+ * This is the main class.
+ *
+ * @author gigadot
+ */
+public class ChatBotRunner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        System.out.println("Zork: Hello, I am Mr. Bot.");
+        while (!quit) {
+            System.out.print("You: ");
+            String name = scanner.nextLine();
+            quit = name.equals("quit");
+            if (!quit) {
+                System.out.println("Zork: " + name);
+            }
+        }
+    }
+
+}
